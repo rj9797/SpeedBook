5b4fa7dca3f69bf817bcbc7dce751119de7e1ba0
Rushabh Joshi <rushabh.joshi97@gmail.com>
2024-09-18 23:42:43 +0530

    .

---
+++ src/main/java/com/irctc/Common/Dump.java
@@ -0,0 +1,18 @@
+package com.irctc.common;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class Dump {
+	@PostConstruct
+	public void start() {
+		int[] arr = new int[]{73,74,75,71,69,72,76,73};
+	}
+	
+	public int[] dailyTemperatures(int[] temperatures) {
+		
+	}
+	
+}
