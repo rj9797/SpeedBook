4becf885f6e43698bc43f3fedcfc017d8443cd08
Rushabh Joshi <rushabh.joshi97@gmail.com>
2024-10-24 22:02:25 +0530

    updated change

--- src/main/java/com/irctc/Common/Dump.java
+++ src/main/java/com/irctc/Common/Dump.java
@@ -1,18 +1,43 @@
 package com.irctc.common;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
 
-@Service
 public class Dump {
-	@PostConstruct
+	
+	
 	public void start() {
-		int[] arr = new int[]{73,74,75,71,69,72,76,73};
+		System.out.println('z'-1);
+		canConstruct("hel","heee");
 	}
 	
-	public int[] dailyTemperatures(int[] temperatures) {
-		 
-	}
-	
+	 public boolean canConstruct(String ransomNote, String magazine) {
+		 Map<Character,Integer> map = new HashMap<Character,Integer>();
+		 char[] charArr = ransomNote.toCharArray();
+		 for(char c : charArr) {
+			 Integer a = map.get((int)c);
+			 if(a!=null) {
+				 map.put(c, a++);
+				 continue;
+			 }
+			 map.put(c, 1);
+		 }
+		 System.out.println("Map beforee: "+map);
+		 for(char c: magazine.toCharArray()) {
+			 map.computeIfPresent(c, (key,value) -> value-11);
+		 }
+		 System.out.println("Map afterr : "+map);
+		 return false;
+	 }
+	 
+	 public static void main(String[] args) {
+		 Dump d = new Dump();
+		 d.start();
+	 }
 }
